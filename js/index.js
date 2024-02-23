bf71301e74c9be64e9314101e2ecfa75a4b6e592
ArifzynXD <arifzyn906@gmail.com>
2024-02-23 17:31:13 +0700

    website

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll(".card-buttons button");
+const sections = document.querySelectorAll(".card-section");
+const card = document.querySelector(".card");
+const handleButtonClick = e => {
+  const targetSection = e.target.getAttribute("data-section");
+  const section = document.querySelector(targetSection);
+  targetSection !== "#about" ?
+    card.classList.add("is-active") :
+    card.classList.remove("is-active");
+  card.setAttribute("data-state", targetSection);
+  sections.forEach(s => s.classList.remove("is-active"));
+  buttons.forEach(b => b.classList.remove("is-active"));
+  e.target.classList.add("is-active");
+  section.classList.add("is-active");
+};
+buttons.forEach(btn => {
+  btn.addEventListener("click", handleButtonClick);
+});
+
+
+function toggleSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.left = sidebar.style.left === "0px" ? "-300px" : "0";
+  sidebar.style.display = "block"
+}
+
+function closeSidebar() {
+  document.querySelector('.sidebar').style.left = "-300px";
+}
